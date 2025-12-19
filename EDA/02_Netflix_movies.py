423abcf20edf7d8c4726b7adac868930676dc33f
Yan Guo <yan.guo@nearfieldinstruments.com>
2025-12-19 15:15:16 +0100

    Added content types and duration analysis

--- EDA/02_Netflix_movies.py
+++ EDA/02_Netflix_movies.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import calendar
 import seaborn as sns
 from viz_utils import *
+from scipy import stats
 
 sns.set_style('darkgrid')
 
@@ -125,5 +126,100 @@ shows_sorted = (
     [['title', 'release_year']]
 )
 print('\nFive oldest TV shows', shows_sorted.head())
+
+#%% Explore content across countries
+top = 20
+df['country_split'] = df['country'].str.split(',')
+df_explode = df.explode(['country_split'])
+df_explode['country_split'] = df_explode['country_split'].str.strip()
+content_per_country = (
+    df_explode
+    .groupby('country_split')
+    .size()
+    .reset_index(name='num_contents')
+)
+content_per_country = content_per_country.sort_values('num_contents', ascending=False)
+
+fig = plt.figure(figsize=(6, 6))
+sns.barplot(data=content_per_country.head(top), x='num_contents', y='country_split',
+            hue='num_contents', legend=None, width=.8)
+plt.xlabel('Number of contents')
+plt.ylabel('')
+plt.title(f'Top {top} countries with the most contents')
+plt.show()
+
+#%% Explore movie durations and TV show seasons
+df_movie = df.query("type == 'Movie'").reset_index(drop=True)
+df_movie['duration_minutes'] = (
+    df_movie['duration']
+    .str.replace(' min', '', regex=False)
+    .astype(float)
+)
+duration = df_movie['duration_minutes']
+mu, sigma = stats.norm.fit(duration)
+x = np.linspace(duration.min(), duration.max(), 200)
+y = stats.norm.pdf(x, mu, sigma)
+
+fig = plt.figure()
+sns.histplot(df_movie, x='duration_minutes', kde=False, stat='density')
+plt.plot(x, y, c='g', lw=2, ls='--', label=f'Mean: {mu:.1f} mins')
+plt.xlabel('Duration [mins]')
+plt.ylabel('Number of movies')
+plt.title('Histogram of movie duration')
+plt.legend()
+plt.show()
+
+longest_movies = (
+    df_movie
+    .sort_values(by='duration_minutes', ascending=False)
+    [['title', 'release_year', 'country', 'duration_minutes']]
+)
+print('\nFive longest movies', longest_movies.head())
+
+df_show = df.loc[df['type'] == 'TV Show'].reset_index(drop=True)
+df_show['season'] = (
+    df_show['duration']
+    .str.extract(r'(\d+)')
+    .astype(int)
+)
+fig = plt.figure()
+sns.histplot(df_show, x='season', binwidth=1)
+plt.xlabel('Number of seasons')
+plt.ylabel('Number of TV shows')
+plt.title('Histogram of TV show seasons')
+plt.show()
+
+longest_show = (
+    df_show
+    .sort_values(by='season', ascending=False)
+    [['title', 'release_year', 'country', 'season']]
+)
+print('\nFive longest shows', longest_show.head())
+
+#%% Explore content types
+ratings = df.groupby(['rating', 'type']).size().reset_index(name='num_contents')
+ratings['type'] = pd.Categorical(
+    ratings['type'],
+    categories=ref_type,
+    ordered=True
+)
+fig = plt.figure()
+sns.barplot(data=ratings, x='rating', y='num_contents', hue='type')
+plt.xlabel('')
+plt.ylabel('Number of contents')
+plt.xticks(rotation=45)
+plt.show()
+
+df['listed_in_split'] = df['listed_in'].str.split(',')
+df_explode = df.explode(column='listed_in_split')
+df_explode['listed_in_split'] = df_explode['listed_in_split'].str.strip()
+df_movie = df_explode.query("type == 'Movie'")
+top = 15
+movie_types = df_movie['listed_in_split'].value_counts().head(top)
+fig = plt.figure()
+sns.barplot(y=movie_types.index, x=movie_types.values, hue=movie_types.values)
+plt.ylabel('')
+plt.xlabel('Number of movies')
+plt.title(f'Top {top} popular movie types')
 
 #%%
