67b05bd24ddf81d89e5e5fede9c8bc6954df58da
Yan Guo <silveryy0528@gmail.com>
2026-01-19 16:59:59 +0100

    trained on full data for Kaggle submission

--- ML/housing_prices.py
+++ ML/housing_prices.py
@@ -1,6 +1,7 @@
 #%%
 import copy
 import sys
+import os
 sys.path.insert(0, '../ML')
 
 import pandas as pd
@@ -17,12 +18,10 @@ from sklearn.pipeline import Pipeline
 import utils
 random_state = 1
 
-
 #%%
 train_data_path = '../ML/housing_prices_train.csv'
 df = pd.read_csv(train_data_path)
 
-#%%
 train_df = copy.deepcopy(df)
 
 corr = utils.select_high_corr_features(train_df, lower_bound=0.25, print_corr=False)
@@ -60,9 +59,11 @@ preprocessor = ColumnTransformer([
 ])
 
 rf_model = RandomForestRegressor(
-    n_estimators=100,
+    n_estimators=600,
     max_depth=None,
     min_samples_split=2,
+    min_samples_leaf=5,
+    max_features=0.7,
     random_state=random_state,
     n_jobs=-1)
 
@@ -81,23 +82,20 @@ print("Train MAE:", mean_absolute_error(y_train, rf_train_preds))
 print("Valid MAE:", mean_absolute_error(y_val, rf_val_preds))
 
 scores = cross_val_score(pipeline, X, y, cv=5, scoring='neg_mean_absolute_error')
-print(f'CV score: {-1 * np.mean(scores)}')
+print(f"CV score: {-scores.mean()}")
 
-# #%%
-# rf_model_on_full_data = RandomForestRegressor(random_state=random_state, max_depth=3)
-# rf_model_on_full_data.fit(X, y)
+#%%
+pipeline.fit(X, y)
 
-# test_data_path = '../ML/housing_prices_test.csv'
-# test_df = pd.read_csv(test_data_path)
+test_data_path = '../ML/housing_prices_test.csv'
+test_df = pd.read_csv(test_data_path)
 
-# #%%
-# X_test = test_df[features]
-# y_test = test_df.SalePrice
+X_test = test_df[features]
 
-# test_preds = rf_model_on_full_data.predict(X_test)
-# print(test_preds[:5])
-# rf_test_mae = mean_absolute_error(test_preds, y_test)
+test_preds = pipeline.predict(X_test)
 
-# print("Test MAE for Random Forest Model: {:,.0f}".format(rf_test_mae))
-
+data = {'Id': test_df.Id, 'SalePrice': test_preds}
+df_to_save = pd.DataFrame(data)
+outfile = '../ML/housing_prices_prediction.csv'
+df_to_save.to_csv(outfile, sep=',', index=False)
 
